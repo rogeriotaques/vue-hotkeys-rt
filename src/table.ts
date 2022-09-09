3994e35c84fee5259da14f4d325f647ccbaf1628
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2022-09-09 23:00:14 +0530

    Added table

---
+++ src/table.ts
@@ -0,0 +1,160 @@
+import Vue from 'vue/dist/vue.js';
+
+
+new Vue({
+    el: "#table-app",
+    data: {
+      keys: [
+        {
+            name: "Arrow Left ⬅️",
+            prop: "ArrowLeft",
+            code: 37
+        },
+        {
+            name: "Arrow Up ⬆️",
+            prop: "ArrowUp",
+            code: 38
+        },
+        {
+            name: "Arrow Right ➡️",
+            prop: "ArrowRight",
+            code: 39
+        },
+        {
+            name: "Arrow Down ⬇️",
+            prop: "ArrowDown",
+            code: 40
+        },
+        {
+          name: "A 🅰️",
+          prop: "A",
+          code: 65
+        },
+        {
+          name: "B 🅱️",
+          prop: "B",
+          code: 66
+        },
+        {
+            name: "C",
+            prop: "C",
+            code: 67
+        },
+        {
+            name: "D",
+            prop: "D",
+            code: 68
+        },
+        {
+            name: "E",
+            prop: "E",
+            code: 69
+        },
+        {
+            name: "F ",
+            prop: "F",
+            code: 70
+        },
+        {
+            name: "G",
+            prop: "G",
+            code: 71
+        },
+        {
+            name: "H",
+            prop: "H",
+            code: 72
+        },
+        {
+            name: "I",
+            prop: "I",
+            code: 73
+        },
+        {
+            name: "J",
+            prop: "J",
+            code: 74
+        },
+        {
+            name: "K",
+            prop: "K",
+            code: 75
+        },
+        {
+            name: "L",
+            prop: "L",
+            code: 76
+        },
+        {
+            name: "M",
+            prop: "M",
+            code: 77
+        },
+        {
+            name: "N",
+            prop: "N",
+            code: 78
+        },
+        {
+            name: "O",
+            prop: "O",
+            code: 79
+        },
+        {
+            name: "P",
+            prop: "P",
+            code: 80
+        },
+        {
+            name: "Q",
+            prop: "Q",
+            code: 81
+        },
+        {
+            name: "R",
+            prop: "R",
+            code: 82
+        },
+        {
+            name: "S",
+            prop: "S",
+            code: 83
+        },
+        {
+            name: "T",
+            prop: "T",
+            code: 84
+        },
+        {
+            name: "U",
+            prop: "U",
+            code: 85
+        },
+        {
+            name: "V",
+            prop: "V",
+            code: 86
+        },
+        {
+            name: "W",
+            prop: "W",
+            code: 87
+        },
+        {
+            name: "X",
+            prop: "X",
+            code: 88
+        },
+        {
+            name: "Y",
+            prop: "Y",
+            code: 89
+        },
+        {
+            name: "Z",
+            prop: "Z",
+            code: 90
+        },
+      ]
+    }
+  })
